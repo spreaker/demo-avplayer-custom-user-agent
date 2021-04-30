e8c12d07a9bd1357c621f271b206ab26c29fc777
Alessandro Calzavara <alessandro.calzavara@gmail.com>
2021-04-30 12:49:59 +0200

    remove unnecessary example

--- AVPlayerCustomUserAgent/ViewController.swift
+++ AVPlayerCustomUserAgent/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     @IBOutlet weak var timeLabel: UILabel!
     
-    var avplayer = AVPlayer()
+    var player = AVPlayer()
     var playableURLString: String {
         if let text = urlField.text, !text.isEmpty {
             return text
@@ -88,31 +88,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let url = URL(string: playableURLString)!
         let playerItem = AVPlayerItem(url: url)
         
-        avplayer.replaceCurrentItem(with: playerItem)
-        avplayer.play()
-    }
-    
-    /**
-     * Usage of AVPlayer API with a custom loader. Code taken from
-     * https://github.com/johnspurlock/swift-playgrounds/blob/master/avplayer-override-user-agent.swift
-     * Not recommended.
-     *
-     * PRO:
-     * - Network requests contains the customized User-Agent "MyPodcastApp", so the listen is correctly attributed to your app.
-     * CONS:
-     * - It  changes "how" content is fetched so much that it downloads the whole audio file before playing it. This is not streaming anymore.
-     */
-    @IBAction func demo2AVPlayerCustomLoader() {
-        print("> AVPlayer Custom Loader ------------------------------------------------------- ")
-        print(playableURLString)
-        
-        let url = URL(string: playableURLString)!
-        let asset = AVURLAsset(url: URL(string: "custom-\(url.absoluteString)")!)
-        asset.resourceLoader.setDelegate(customLoaderDelegate, queue: .global(qos: .default))
-        let playerItem = AVPlayerItem(asset: asset)
-        
-        avplayer.replaceCurrentItem(with: playerItem)
-        avplayer.play()
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
     }
     
     /**
@@ -133,38 +110,38 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let asset = AVURLAsset(url: url, options: ["AVURLAssetHTTPHeaderFieldsKey": headers])
         let playerItem = AVPlayerItem(asset: asset)
         
-        avplayer.replaceCurrentItem(with: playerItem)
-        avplayer.play()
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
     }
 
     @IBAction func reset() {
         removePeriodicTimeObserver()
-        avplayer.pause()
+        player.pause()
         
-        avplayer = AVPlayer()
+        player = AVPlayer()
         addPeriodicTimeObserver()
         
         print("> RESET ------------------------------------------------------- ")
     }
     
     @IBAction func seekBack() {
-        let newTime = avplayer.currentTime().timeWithOffset(-30)
+        let newTime = player.currentTime().timeWithOffset(-30)
         if (newTime.isValid) {
-            avplayer.seek(to: newTime)
+            player.seek(to: newTime)
         }
     }
     
     @IBAction func seekForward() {
-        let newTime = avplayer.currentTime().timeWithOffset(+30)
+        let newTime = player.currentTime().timeWithOffset(+30)
         if (newTime.isValid) {
-            avplayer.seek(to: newTime)
+            player.seek(to: newTime)
         }
     }
     
     @IBAction func seekForwardMinutes() {
-        let newTime = avplayer.currentTime().timeWithOffset(+5*60)
+        let newTime = player.currentTime().timeWithOffset(+5*60)
         if (newTime.isValid) {
-            avplayer.seek(to: newTime)
+            player.seek(to: newTime)
         }
     }
 }
